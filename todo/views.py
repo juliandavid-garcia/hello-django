a4ea70594f3f7d042a3c18232d1ba78b61356767
juliandavid-garcia <juliandavid.garcia@gmail.com>
2021-11-17 20:03:13 +0000

    added the delect item functionality

--- todo/views.py
+++ todo/views.py
@@ -23,7 +23,8 @@ def add_item(request):
     }
     return render(request, "todo/add_item.html", context)
 
-def edit_item( request, item_id):
+
+def edit_item(request, item_id):
     item = get_object_or_404(Item, id=item_id)
     if request.method == "POST":
         form = ItemForm(request.POST, instance=item)
@@ -35,3 +36,16 @@ def edit_item( request, item_id):
         'form': form
     }
     return render(request, 'todo/edit_item.html', context)
+
+
+def toggle_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.done = not item.done
+    item.save()
+    return redirect('get_todo_list')
+
+
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.delete()
+    return redirect('get_todo_list')
